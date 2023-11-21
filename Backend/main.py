655799c907a15a1f2e3c391380ebddeba772a80d
Jayem-11 <ronojohnmichael@gmail.com>
2023-11-21 12:57:35 +0300

    updates

--- Backend/main.py
+++ Backend/main.py
@@ -1,22 +1,33 @@
-
 from typing import Union
 from fastapi import FastAPI
 from pytube import YouTube
 from moviepy.editor import *
-from speechbrain.pretrained import EncoderASR
 from huggingface_hub import login
-from transformers import AutoTokenizer , AutoModelForSeq2SeqLM , pipeline
+from transformers import AutoTokenizer , AutoModelForSeq2SeqLM , pipeline, AutoModel
+from transformers import WhisperForConditionalGeneration, WhisperProcessor
+from transformers import WhisperFeatureExtractor, WhisperTokenizer
+from fastapi import FastAPI, UploadFile
+from starlette.responses import JSONResponse
+import soundfile as sf
+import librosa
+import numpy as np
+import io
+import tempfile
 
-
-secret_value = "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"
-login(token=secret_value)
-
-t5_tokenizer = AutoTokenizer.from_pretrained("google/mt5-small")
-asr_model = EncoderASR.from_hparams(source="speechbrain/asr-wav2vec2-dvoice-swahili", savedir="pretrained_models/asr-wav2vec2-dvoice-swahili")
-model = (AutoModelForSeq2SeqLM.from_pretrained("./model/summary") ) 
-
+# secret_value = "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"
+# login(token=secret_value)
 
 app = FastAPI()
+
+feature_extractor = WhisperFeatureExtractor.from_pretrained("openai/whisper-small")
+tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-small", language="Swahili", task="transcribe")
+processor = WhisperProcessor.from_pretrained("Jayem-11/whisper-small-swahili-3")
+asr_model = AutoModel.from_pretrained('../SpeechToText/model')
+forced_decoder_ids = processor.get_decoder_prompt_ids(language="sw", task="transcribe")
+
+t5_tokenizer = AutoTokenizer.from_pretrained("google/mt5-small")
+summary_model = (AutoModelForSeq2SeqLM.from_pretrained("../TextSummarization/model")) 
+
 
 @app.get("/")
 def read_root():
@@ -32,60 +43,47 @@ def download_YTvideo(video_url: str):
     # yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
 
 
-def extract_audio():
-    video = VideoFileClip("myHolidays.mp4").subclip(50, 60)
+def extract_and_resample_audio(file):
 
-    # Make the text. Many more options are available.
-    txt_clip = (TextClip("My Holidays 2013", fontsize=70, color='white')
-                .set_position('center')
-                .set_duration(10))
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".mp4")
+    temp_file.write(file)
+    temp_file.close()
 
-    result = CompositeVideoClip([video, txt_clip])  # Overlay text on video
-    result.write_videofile("myHolidays_edited.webm", fps=25)  # Many options...
+    # Load the video file from the uploaded file
+    video = VideoFileClip(temp_file.name)
 
+    # Extract audio from the video
+    audio = video.audio
 
+    # Save the audio to a temporary file
+    audio.write_audiofile("temp_audio.wav")
+
+    # Load the temporary audio file
+    audio_data, sr = sf.read("temp_audio.wav")
+
+    # Resample the audio to 16000Hz
+    audio_resampled = librosa.resample(audio_data, orig_sr = sr, target_sr=16000)
+    print("Done resampling")
+
+    return audio_resampled
 
 @app.post("/predict")
-def predict():
-    audio = extract_audio()
-    sample_text = asr_model.transcribe_file(audio)
-    summarizer = pipeline("summarization", model=model, tokenizer=t5_tokenizer)
+async def predict(file: UploadFile):
+    audio_resampled = extract_and_resample_audio(await file.read())
 
-    summarizer(
+    input_feats = feature_extractor(audio_resampled, sampling_rate = 16000).input_features[0]
+    input_feats = np.expand_dims(input_feats, axis=0)
+
+    output = asr_model.generate(input_feats)
+
+    sample_text = tokenizer.batch_decode(output, skip_special_tokens=True)
+
+
+    summarizer = pipeline("summarization", model=summary_model, tokenizer=t5_tokenizer)
+
+    summary = summarizer(
         sample_text,
         max_length=215,
     )
 
-
-# from typing import Union
-# from fastapi import FastAPI
-# from pytube import YouTube
-# from moviepy.editor import *
-
-# app = FastAPI()
-
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-
-
-# @app.post("/download")
-# def download_YTvideo(video_url: str):
-#     YouTube(video_url).streams.first().download()
-#     return {"Successful"}
-
-#     # yt = YouTube('http://youtube.com/watch?v=2lAe1cqCOXo')
-#     # yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
-
-
-# @app.post("/extract_audio")
-# def extract_audio():
-#     video = VideoFileClip("myHolidays.mp4").subclip(50, 60)
-
-#     # Make the text. Many more options are available.
-#     txt_clip = (TextClip("My Holidays 2013", fontsize=70, color='white')
-#                 .set_position('center')
-#                 .set_duration(10))
-
-#     result = CompositeVideoClip([video, txt_clip])  # Overlay text on video
-#     result.write_videofile("myHolidays_edited.webm", fps=25)  # Many options...
+    return {'summary': summary,}
